2641e4e27cc26457268580273586e218d8468a95
Daniel Horowitz <daniel.horowitz@n26.com>
2021-08-14 22:10:15 +0200

    Add api call when starting activity

--- android/app/src/main/java/com/dhorowitz/openmovie/OpenMovieApplication.kt
+++ android/app/src/main/java/com/dhorowitz/openmovie/OpenMovieApplication.kt
@@ -2,6 +2,16 @@ package com.dhorowitz.openmovie
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 @HiltAndroidApp
-class OpenMovieApplication: Application()
+class OpenMovieApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        when {
+            BuildConfig.DEBUG -> Timber.plant(DebugTree())
+        }
+    }
+}
--- android/app/src/main/java/com/dhorowitz/openmovie/discover/presentation/DiscoverViewModel.kt
+++ android/app/src/main/java/com/dhorowitz/openmovie/discover/presentation/DiscoverViewModel.kt
@@ -1,22 +1,29 @@
 package com.dhorowitz.openmovie.discover.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dhorowitz.openmovie.discover.domain.GetPopularMovies
 import com.dhorowitz.openmovie.discover.presentation.model.DiscoverAction
 import com.dhorowitz.openmovie.discover.presentation.model.DiscoverAction.Load
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class DiscoverViewModel @Inject constructor() : ViewModel() {
-
-
+class DiscoverViewModel @Inject constructor(
+    private val getPopularMovies: GetPopularMovies
+) : ViewModel() {
 
     fun handle(action: DiscoverAction) = when (action) {
         Load -> fetchPopularMovies()
     }
 
     private fun fetchPopularMovies() {
-
+        viewModelScope.launch {
+            runCatching { getPopularMovies() }
+                .onSuccess { Timber.d("ok") }
+                .onFailure { Timber.e(it) }
+        }
     }
-
 }
