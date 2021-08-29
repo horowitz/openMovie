aa1e01bb76e31d575243162fdef41601a58415e0
Daniel Horowitz <daniel.horowitz@n26.com>
2021-08-29 14:04:58 +0200

    Add tests to open links

--- features/moviedetails/src/androidTest/java/com/dhorowitz/openmovie/moviedetails/MovieDetailsActivityTest.kt
+++ features/moviedetails/src/androidTest/java/com/dhorowitz/openmovie/moviedetails/MovieDetailsActivityTest.kt
@@ -3,6 +3,7 @@ package com.dhorowitz.openmovie.moviedetails
 import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.intent.Intents
 import androidx.test.platform.app.InstrumentationRegistry
 import com.dhorowitz.openmovie.moviedetails.data.MovieApi
 import com.dhorowitz.openmovie.moviedetails.di.MovieDetailsNetworkModule
@@ -34,6 +35,7 @@ class MovieDetailsActivityTest {
 
     @Before
     fun setUp() {
+        Intents.init()
         hiltRule.inject()
         mockWebServer = MockWebServer()
         mockWebServer.dispatcher = MockServerDispatcher()
@@ -43,29 +45,50 @@ class MovieDetailsActivityTest {
     @After
     fun tearDown() {
         mockWebServer.shutdown()
+        Intents.release()
     }
 
     @Test
     fun shouldDisplayItemsCorrectly() {
-        val movieId = "123"
-        val intent: Intent = Intent(targetContext, MovieDetailsActivity::class.java).apply {
-            putExtra("id", movieId)
-        }
-
-        givenItemsFromNetwork(movieId)
-
-        ActivityScenario.launch<MovieDetailsActivity>(intent)
+        setupDetailsAndLaunch()
 
         movieDetailsRobot {
             areItemsDisplayedCorrectly()
         }
     }
 
-    private fun givenItemsFromNetwork(movieId: String) {
+    @Test
+    fun shouldOpenHomepageAfterClick() {
+        setupDetailsAndLaunch()
+
+        movieDetailsRobot {
+            clickOnHomepageButton()
+            didOpenHomepageLink()
+        }
+    }
+
+    @Test
+    fun shouldOpenIMDBAfterClick() {
+        setupDetailsAndLaunch()
+
+        movieDetailsRobot {
+            clickOnIMDBButton()
+            didOpenIMDBLink("tt6334354")
+        }
+    }
+
+    private fun setupDetailsAndLaunch(){
+        val movieId = "123"
+        val intent: Intent = Intent(targetContext, MovieDetailsActivity::class.java).apply {
+            putExtra("id", movieId)
+        }
+
         mockWebServer.registerApiRequest(
             HttpRequest("/${MovieApi.PATH}/$movieId", HttpMethod.GET),
             getJsonContent("movie-details-200.json")
         )
+
+        ActivityScenario.launch<MovieDetailsActivity>(intent)
     }
 
     private fun getJsonContent(fileName: String): String =
--- features/moviedetails/src/androidTest/java/com/dhorowitz/openmovie/moviedetails/MovieDetailsRobot.kt
+++ features/moviedetails/src/androidTest/java/com/dhorowitz/openmovie/moviedetails/MovieDetailsRobot.kt
@@ -1,6 +1,16 @@
 package com.dhorowitz.openmovie.moviedetails
 
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn
+import org.hamcrest.Matcher
+import org.hamcrest.core.AllOf.allOf
 
 
 internal fun movieDetailsRobot(func: MovieDetailsRobot.() -> Unit) =
@@ -13,5 +23,28 @@ class MovieDetailsRobot {
         assertDisplayed("Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.")
         assertDisplayed("⭐️ 8.1")
         assertDisplayed("🕒 132 min")
+    }
+
+    fun clickOnHomepageButton() {
+        clickOn(R.id.homepageButton)
+    }
+
+    fun clickOnIMDBButton() {
+        clickOn(R.id.imdbButton)
+    }
+
+
+    fun didOpenIMDBLink(imdbId: String) {
+        val expectedIntent: Matcher<Intent> =
+            allOf(hasAction(Intent.ACTION_VIEW), hasData("https://www.imdb.com/title/$imdbId"))
+        intending(expectedIntent).respondWith(Instrumentation.ActivityResult(0, null))
+        intended(expectedIntent)
+    }
+
+    fun didOpenHomepageLink() {
+        val expectedIntent: Matcher<Intent> =
+            allOf(hasAction(Intent.ACTION_VIEW), hasData("https://www.thesuicidesquad.net"))
+        intending(expectedIntent).respondWith(Instrumentation.ActivityResult(0, null))
+        intended(expectedIntent)
     }
 }
