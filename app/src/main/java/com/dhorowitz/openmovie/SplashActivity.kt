a5d98b7fd053598fe484ee501534a906e31521dd
Daniel Horowitz <daniel.horowitz@n26.com>
2021-08-31 18:06:55 +0200

    Splash screen and launcher icon

--- app/src/main/java/com/dhorowitz/openmovie/SplashActivity.kt
+++ app/src/main/java/com/dhorowitz/openmovie/SplashActivity.kt
@@ -1,14 +1,19 @@
 package com.dhorowitz.openmovie
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.dhorowitz.openmovie.navigation.navigateTo
 import com.gaelmarhic.quadrant.QuadrantConstants
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        navigateTo(QuadrantConstants.DISCOVER_ACTIVITY)
-        finish()
+        setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateTo(QuadrantConstants.DISCOVER_ACTIVITY)
+            finish()
+        }, 3000)
     }
 }
