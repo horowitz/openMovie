8dcc8c564818b8ccf46bdb9f3970ccd473eadaf5
Daniel Horowitz <daniel.horowitz@n26.com>
2021-09-01 12:13:53 +0200

    Add title animation for SplashActivity

--- app/src/main/java/com/dhorowitz/openmovie/SplashActivity.kt
+++ app/src/main/java/com/dhorowitz/openmovie/SplashActivity.kt
@@ -3,6 +3,10 @@ package com.dhorowitz.openmovie
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.annotation.AnimRes
 import androidx.appcompat.app.AppCompatActivity
 import com.dhorowitz.openmovie.navigation.navigateTo
 import com.gaelmarhic.quadrant.QuadrantConstants
@@ -11,9 +15,15 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.appNameTextView).startAnimation(R.anim.zoom_in)
         Handler(Looper.getMainLooper()).postDelayed({
             navigateTo(QuadrantConstants.DISCOVER_ACTIVITY)
             finish()
         }, 3000)
     }
+}
+
+private fun TextView.startAnimation(@AnimRes animRes: Int) {
+    visibility = View.VISIBLE
+    startAnimation(AnimationUtils.loadAnimation(context, animRes))
 }
